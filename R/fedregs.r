17f441515809549db01af56fb9155f8d17986267
slarge <largesi@gmail.com>
2018-07-02 12:29:53 -0400

    update documentation to satisfy R CMD checks

---
+++ R/fedregs.r
@@ -0,0 +1,16 @@
+#' \code{fedregs} package
+#'
+#' A Package to Facilitate Text Analysis of the US Code of Federal Regulations
+#'
+#' See the README on
+#' \href{https://github.com/slarge/fedregs/blob/master/README.md}{GitHub}
+#'
+#' @docType package
+#' @name fedregs
+#' @importFrom magrittr %>%
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "parts", "chapters",
+                                                        "min_parts", "max_parts", "SECTION_NUMBER",
+                                                        "TEXT", "SUBPART_NAME", "word", "subpart"))
